e651ae7aa8bc0866437c0ba9d313bc15dcf04bc8
Lorenzo Kniss <lorenzo.kniss@gmail.com>
2025-02-06 19:41:33 +1000

    Add: Create a new local scope for Application model to allow filtering records by the plan type. Added a unit test to cover this scenario

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Enums\ApplicationStatus;
 use App\Events\ApplicationCreated;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -23,5 +24,13 @@ class Application extends Model
     public function plan(): BelongsTo
     {
         return $this->belongsTo(Plan::class);
+    }
+
+    /**
+     * Scope a query to only include applications with a plan of a given type.
+     */
+    public function scopePlanType(Builder $query, string $planType): Builder
+    {
+        return $query->whereHas('plan', fn ($query) => $query->where('type', $planType));
     }
 }
--- tests/Unit/Models/ApplicationTest.php
+++ tests/Unit/Models/ApplicationTest.php
@@ -14,4 +14,23 @@ class ApplicationTest extends TestCase
 
         $this->assertInstanceOf(Plan::class, $application->plan);
     }
+
+    public function test_should_return_the_correct_applications_when_filtering_by_plan_type(): void
+    {
+        // Create two applications with different plan types
+        $nbnApplication = Application::factory()->for(Plan::factory()->nbn())->create();
+        $mobileApplication = Application::factory()->for(Plan::factory()->mobile())->create();
+
+        // Filter by `nbn` plan type
+        $result = Application::planType('nbn')->get();
+
+        $this->assertCount(1, $result);
+        $this->assertEquals($nbnApplication->id, $result->first()->id);
+
+        // Filter by `mobile` plan type
+        $result = Application::planType('mobile')->get();
+
+        $this->assertCount(1, $result);
+        $this->assertEquals($mobileApplication->id, $result->first()->id);
+    }
 }
