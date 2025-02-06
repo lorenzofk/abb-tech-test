49de5406c5a1500bc32e6e4cdc405b59f8a338c0
Lorenzo Kniss <lorenzo.kniss@gmail.com>
2025-02-06 21:21:04 +1000

    Add: Create the 'applications' relationship and the accessor for the monthly cost in the Plan model. Add two new test cases to cover both scenarios

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Plan extends Model
 {
     use HasFactory;
+
+    public function applications(): HasMany
+    {
+        return $this->hasMany(Application::class);
+    }
+
+    /**
+     * Get the monthly cost of the plan in human readable format.
+     */
+    protected function monthlyCostFormatted(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => number_format($this->monthly_cost / 100, 2),
+        );
+    }
 }
