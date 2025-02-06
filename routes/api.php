bcc7caa72d9ce329c864f066dba5e405f3ce73af
Lorenzo Kniss <lorenzo.kniss@gmail.com>
2025-02-06 21:24:03 +1000

    Refactor: group both api endpoints in the 'auth:sanctum' middleware

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ApplicationController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::middleware('auth:sanctum')->group(function () {  
+    Route::get('/user', fn () => auth()->user());
     Route::get('/applications', [ApplicationController::class, 'index'])->name('api.applications');
 });
